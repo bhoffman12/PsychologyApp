71d562309b2e96505c29d19eb779d182a49bc24f
bhoffman12 <105113587+bhoffman12@users.noreply.github.com>
2022-12-11 21:22:55 -0500

    Delete node-doctor-appointment-master directory

--- node-doctor-appointment-master/public/js/index.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-
-    console.log('jkhkhkh')
-    const jhjhj = document.getElementById('kjhkjhkjh');
-
-    jhjhj.style.display = 'none';
-}, 4000);
-
-console('----------')
--- node-doctor-appointment-master/test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Update = require('./models/Updates')
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:
-true});
-
-var id = "62b27ca8374dbc665eb21665";
-Update.findByIdAndDelete(id, (error, update) =>{
-    console.log(error,update)
-})
